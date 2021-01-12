2f6c031d8805e300178ce12301389e20a370e948
sungyoonahn <51948435+sungyoonahn@users.noreply.github.com>
2021-01-12 13:24:54 +0900

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device:{}'.format(tf.test.gpu_device_name()))
+
+else:
+    print("Please install GPU version of TF")
+
+print(tf.__version__)
+
